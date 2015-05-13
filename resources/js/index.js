aac3d8d6c7cc43a26bb3aba980f24fcf682cceba
Richard Xue <liming.snwoy@gmail.com>
2015-05-13 17:54:42 +0800

    add demo1

---
+++ resources/js/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 05/13/2015.
+ */
+$(function(){
+    "use strict";
+    var index = {};
+    index.init = function() {
+        //$("#demoBtn").click(function(){
+        //    alert(">>>>>");
+        //});
+    };
+    index.init();
+});
